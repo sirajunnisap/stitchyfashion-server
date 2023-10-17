bdaee3668298bbbc8405cf8b51d62da4d403aa76
sirajunnisa <sirajunnisap@gmail.com>
2023-10-17 16:33:05 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,10 @@ const app: Application = express()
 
 
 //Enable CORS for all routes
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://stitchy-inky.vercel.app'],
+  }));
+  
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '500mb' }));
 
