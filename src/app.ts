644a520e2fdaa61916d6de972575d2d5d1752075
sirajunnisa <sirajunnisap@gmail.com>
2023-10-18 15:00:27 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const app: Application = express()
 
 //Enable CORS for all routes
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://stitchy-inky.vercel.app','stitchy-3gkkacsoq-sirajunnisas-projects.vercel.app'],
+    origin: ['http://localhost:3000', 'https://stitchy-inky.vercel.app','https://stitchy-git-main-sirajunnisas-projects.vercel.app/'],
   }));
   
 app.use(express.json({ limit: '500mb' }));
@@ -63,7 +63,7 @@ const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
         origin: ['https://stitchy-inky.vercel.app'
-    ,'stitchy-3gkkacsoq-sirajunnisas-projects.vercel.app'
+    ,'https://stitchy-git-main-sirajunnisas-projects.vercel.app/'
 ]
 
     },
