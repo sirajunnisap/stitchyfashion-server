026faf971cc52f98d3ddbdcca34e9b6172c9b65f
sirajunnisa <sirajunnisap@gmail.com>
2023-10-18 17:03:32 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,14 @@ app.use(cors({
     // origin: ['http://localhost:3000', 'https://stitchy-inky.vercel.app','https://stitchy-git-main-sirajunnisas-projects.vercel.app'],
     origin: "*"
 }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://stitchy-inky.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
   
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '500mb' }));
